df8668b50f7c3a1ef66969e7ded73a63c28b0229
superchd <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>
2022-09-03 12:08:37 +0900

    100

--- stairs/stairs.cpp
+++ stairs/stairs.cpp
@@ -9,17 +9,12 @@ vector<string> stairs;
 vector<int> x_stair;
 vector<int> y_stair;
 vector<int> whole_stair;
-
 int dx[2] = {0, 1};
 int dy[2] = {-1, 0};
 int dirNum = 0;
-struct point{
-	int x;
-	int y;
-	int more_y;
-};
+struct point{int x;int y;int more_y;};
 vector<point> points;
-vector<point> range_points;
+vector<vector<int>> r_p;
 vector<point> range_arr;
 void init(){
 	int num, _x, _y;
@@ -40,77 +35,53 @@ void init(){
 	return;
 }
 void draw(){
+	int curr_x = 0;
 	int curr_y = 0;
 	int x_sum = accumulate(x_stair.begin(), x_stair.end(), 0);
 	int y_sum = accumulate(y_stair.begin(), y_stair.end(), 0);
+	int repeat;
+	vector<int> c;
 	curr_y = y_sum;
 	point start;
 	start.x = 0; start.y = y_sum;
-	range_points.push_back(start);
+	c.push_back(y_sum);
+	r_p.push_back(c);
 	dirNum = 1;
-	for (int i = 0; i < whole_stair.size() - 1; i++){
+	for (int i = 0; i < whole_stair.size(); i++){
+		point ex;
+		c.clear();
+		for(int j = 0; j < whole_stair[i]; j++){
+			ex.x = curr_x + j * dx[dirNum];
+			ex.y = curr_y + j * dy[dirNum];
+			if (i % 2 == 0 && j != whole_stair[i] - 1) {
+				c.clear();
+				c.push_back(ex.y);
+				r_p.push_back(c);
+			}
+			else {c.push_back(ex.y);}
+		}
+		if (i % 2 == 1){
+		r_p.push_back(c);}
 		curr_x += whole_stair[i] * dx[dirNum];
 		curr_y += whole_stair[i] * dy[dirNum];
 		dirNum = (dirNum + 1) % 2;
-		point ex;
-		ex.x = curr_x;
-		ex.y = curr_y;
-		range_points.push_back(ex);
 	}
-	point end;
-	end.x = x_sum; end.y = 0;
-	range_points.push_back(end);
 }
-void	make_range(){
-	point start;
-	point end;
-	point ex;
-	ex.x = 0;
-	ex.y = accumulate(y_stair.begin(), y_stair.end(), 0);
-	range_arr.push_back(ex);
-	for(int i = 0; i < range_points.size() - 1; i++){
-		if (i % 2 == 0){
-			start.x = range_points[i].x;
-			start.y = range_points[i].y;
-		}
-		else {
-			end.x = range_points[i].x;
-			end.y = range_points[i].y;
-			for (int j = start.x + 1; j <= end.x; j++){
-				ex.x = j;
-				ex.y = start.y;
-				range_arr.push_back(ex);
-			}
-		}	
-	}
-	for (int i = 0; i < range_arr.size() - 1; i++){
-		if (range_arr[i].y != range_arr[i + 1].y){
-			range_arr[i].more_y = range_arr[i + 1].y;
-		}
-		else {range_arr[i].more_y = range_arr[i].y;}	
-	}
-	range_arr[range_arr.size()-1].more_y = 0;
-	return;
-}
-void calculate(point ex){
-	int idx = ex.x;
-	int compare = ex.y;
-	if (range_arr[idx].y == range_arr[idx].more_y){
-		if(range_arr[idx].y > compare) {cout << "in" << endl;}
-		else if (range_arr[idx].y < compare) {cout << "out" << endl;}
-		else {cout << "on" << endl;}
-	}
+void calculate(point p){
+	if (p.x >= r_p.size()) {cout << "out" << endl;return;}
+	int min = *min_element(r_p[p.x].begin(), r_p[p.x].end());
+	int max = *max_element(r_p[p.x].begin(), r_p[p.x].end());
+	if (min - 1 > p.y) {cout << "in" << endl;}
 	else {
-		if(range_arr[idx].y >= compare && range_arr[idx].more_y <= compare) {cout << "on" << endl;}
-		else if (range_arr[idx].more_y > compare) {cout << "in" << endl;}
-		else {cout << "out" << endl;}
+		if (max < p.y) {cout << "out" << endl;}
+		else{cout << "on" <<endl;}
 	}
-	return;
 }
 int main(){
-    ios_base :: sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
     init();
     draw();
-    make_range();
-    for (auto a: points){calculate(a);}
+    for (auto p:points){calculate(p);}
 }
